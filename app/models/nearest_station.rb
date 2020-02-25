199d2473744ad607bfa2b27ea79959499c04bd75
hiroki matsubara <dr5se4aw3@yahoo.com>
2020-02-25 16:35:21 +0900

    Add 最寄駅フォームが未入力の際、最寄駅のパラメータを作成しないようにする。バリデーションを追加。

--- app/models/nearest_station.rb
+++ app/models/nearest_station.rb
@@ -1,3 +1,6 @@
 class NearestStation < ApplicationRecord
   belongs_to :rental_property
+  validates :raleway, presence: true
+  validates :name, presence: true
+  validates :minute, presence: true
 end
--- app/models/rental_property.rb
+++ app/models/rental_property.rb
@@ -1,4 +1,10 @@
 class RentalProperty < ApplicationRecord
   has_many :nearest_stations, dependent: :destroy
-  accepts_nested_attributes_for :nearest_stations
+  accepts_nested_attributes_for :nearest_stations, reject_if: :all_blank
+
+  validates :name, presence: true
+  validates :rent, presence: true
+  validates :address, presence: true
+  validates :age, presence: true
+  validates :note, presence: true
 end
